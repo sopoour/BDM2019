b7c2f5398171bd8622dcee9661861a9c3db5eb5f
Iman Elghandour <ielghand@alexu.edu.eg>
2019-08-30 12:24:16 +0200

    adding dirs for exercises and projecsts

---
+++ Exercises/Exercise1/src/main/scala/package_name/Program_name.scala
@@ -0,0 +1,31 @@
+package package_name
+
+import scala.annotation.tailrec
+
+object Program_name {
+
+    // This is the program entry point
+    def main(args: Array[String]): Unit = {
+        hello()
+    }
+
+    def hello() : Unit = {
+        println("Hello, World!")
+    }
+
+    def factorialTR(n: Int): Int = {
+        //@tailrec
+        //def factorial(n: Int, f: Int): Int = {
+        //  ???
+        //}
+        ???
+    }
+
+    def pascal(c: Int, r: Int): Int = {
+        ???
+    }
+
+    def runpascal(x:Int): Unit = {
+        ???
+    }
+}
