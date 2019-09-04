7fd6270abbb01efe102a10b28aa9816371d34d5b
Jubas <run4real@gmail.com>
2019-09-04 12:28:21 +0200

    Added updated Exercise 2 files

---
+++ Exercises/Exercise2/src/main/scala/BIDMTMath/SimpleMath2.scala
@@ -0,0 +1,12 @@
+
+package BIDMTMath
+
+// Import from the other file specified
+import BIDMTMath.SimpleMath.add
+
+object SimpleMath2 {
+
+  def main(args: Array[String]): Unit = {
+    println(add(19,412))
+  }
+}
