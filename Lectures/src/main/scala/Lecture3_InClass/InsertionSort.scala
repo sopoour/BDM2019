ab7f420339cba7cc40dba85c932b01082aab5ca7
sopoour <sophia.auer@googlemail.com>
2019-09-16 12:51:03 +0200

    Lecture - InsertionSort function

--- Lectures/src/main/scala/Lecture3_InClass/InsertionSort.scala
+++ Lectures/src/main/scala/Lecture3_InClass/InsertionSort.scala
@@ -1,12 +1,21 @@
 package Lecture3_InClass
 
 object InsertionSort {
-
+  def iSort(ls: List[Int]): List[Int] = ls match {
+    case Nil => Nil
+    case x :: xs => insert(x , iSort(xs))
+  }
+  def insert(x:Int, xs:List[Int]): List[Int] =
+    xs match{
+      case Nil => List(x)
+      case y :: ys => if(x<=y) x :: xs
+      else y :: insert(x,ys)
+    }
 
 
   def main(args: Array[String]): Unit = {
     val list = List(2,4,1,6,1,5,0)
-    //println(iSort)
+    println(iSort(list))
   }
 
 
